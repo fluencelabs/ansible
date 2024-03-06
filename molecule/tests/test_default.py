42ec72d2e0f92adbbccfafb175d63c53ca82de9f
Anatoly Laskaris <github_me@nahsi.dev>
2024-03-06 23:19:59 +0200

    Add ccp role and rewrite nox role to allow only one nox per host

---
+++ molecule/tests/test_default.py
@@ -0,0 +1,65 @@
+import testinfra.utils.ansible_runner
+import pytest
+import os
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+        os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts("all")
+
+
+@pytest.mark.parametrize("directory", [
+    "/opt/fluence/nox",
+    "/opt/fluence/ccp",
+])
+def test_directories(host, directory):
+    d = host.file(directory)
+
+    assert d.is_directory
+    assert d.exists
+    assert d.mode == 493
+
+
+@pytest.mark.parametrize("config", [
+    "/opt/fluence/nox/Config.toml",
+    "/opt/fluence/ccp/Config.toml",
+])
+def test_config(host, config):
+    d = host.file(config)
+
+    assert d.is_file
+    assert d.exists
+    assert d.mode == 416
+
+
+def test_ipfs_download(host):
+    f = host.file("/usr/local/bin/ipfs")
+
+    assert f.is_file
+    assert f.exists
+
+
+@pytest.mark.parametrize("service", [
+    "nox",
+    "ccp",
+])
+def test_service_is_running(host, service):
+    service = host.service(service)
+
+    assert service.is_running
+    assert service.is_enabled
+
+
+@pytest.mark.parametrize("user", [
+    "nox",
+    "ccp",
+])
+def test_user_created(host, user):
+    assert host.user(user).exists
+    assert user in host.user(user).groups
+
+
+@pytest.mark.parametrize("package", [
+    "libhwloc-dev",
+    "curl",
+])
+def test_packages_installed(host, package):
+    assert host.package(package).is_installed
---
+++ roles/prerequisites/README.md
@@ -0,0 +1,12 @@
+# fluencelabs.provider.prerequisites
+
+Configure system before running Fluence provider toolkit.
+
+⚠ Attention: this role will modify system by:
+- installing packages
+  - libhwloc-dev - required to get CPU topology
+  - curl - default effector used by Nox
+
+## Author
+
+- **Anatolios Laskaris** - [nahsi](https://github.com/nahsi)
